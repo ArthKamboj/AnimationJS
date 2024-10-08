938b172f03d73e7e1cf3b07d2b55537b17abe43f
Arth Kamboj <arth23153015@akgec.ac.in>
2024-10-08 15:08:58 +0530

    SelectionPage made functional

--- script.js
+++ script.js
@@ -10,4 +10,14 @@ window.onload = function() {
  playb.addEventListener('click', function() {
     window.location.href = 'selector.html'
  })
- 
+
+ //UNCLICKABLE BUTTON KI JS
+//  const wrapper = document.querySelector('.wrap')
+//  const untouch = document.getElementById('yuck')
+//  const unreact = untouch.
+// untouch.addEventListener('mouseover', () => {
+//    const i = Math.floor(Math.random())*window.innerWidth
+//    const j = Math.floor(Math.random())*window.innerHeight
+//    untouch.style.left = i + 'px'
+//    untouch.style.top = j + 'px'
+// })
